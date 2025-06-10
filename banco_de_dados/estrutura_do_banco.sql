0b60173453f66dda5be87b30c293a8ad656a1389
Magnus <rianmagnus5791@gmail.com>
2025-06-10 16:36:33 -0300

    primeiro commit

---
+++ banco_de_dados/estrutura_do_banco.sql
@@ -0,0 +1,118 @@
+CREATE DATABASE clinica 
+character set utf8mb4
+default character set utf8mb4;
+
+USE clinica;
+
+CREATE TABLE pacientes(
+	cpf VARCHAR(14) PRIMARY KEY ,
+    nome VARCHAR(85) NOT NULL,
+    sexo ENUM("masculino","feminino"),
+    senha_de_acesso CHAR(64) NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    data_nascimento DATE NOT NULL
+
+
+
+
+)CHARSET = utf8mb4;
+
+
+CREATE TABLE endereco_paciente(
+	cpf_paciente VARCHAR(14) PRIMARY KEY,
+    cep INT NOT NULL,
+    rua VARCHAR(50) NOT NULL,
+    bairro VARCHAR(30) NOT NULL,
+    numero INT NOT NULL,
+    complemento TEXT(150),
+    CONSTRAINT fk_enderecos_cliente 
+    FOREIGN KEY(cpf_paciente) REFERENCES pacientes(cpf)
+
+
+)CHARSET = utf8mb4;
+
+
+
+
+CREATE TABLE telefone_pacientes(
+	cpf_paciente VARCHAR(14) PRIMARY KEY,
+    telefone VARCHAR(14) NOT NULL,
+    CONSTRAINT fk_telefones_paciente
+    FOREIGN KEY(cpf_paciente) REFERENCES pacientes(cpf)
+
+
+
+
+
+)CHARSET = utf8mb4;
+
+CREATE TABLE medicos(
+	crm VARCHAR(10) PRIMARY KEY,
+    especialidade INT NOT NULL,
+    nome VARCHAR(85) NOT NULL,
+    senha_de_acesso CHAR(64) NOT NULL,
+    email VARCHAR(320) NOT NULL
+    
+
+
+
+
+)CHARSET = utf8mb4;
+
+
+CREATE TABLE disponibilidade(
+
+	id_disponibilidade VARCHAR(16) PRIMARY KEY,
+    especialidade VARCHAR(20) NOT NULL,
+    data_disp DATE NOT NULL,
+    hora TIME NOT NULL
+    
+    
+
+
+
+
+)CHARSET = utf8mb4;
+
+
+CREATE TABLE registra(
+
+	id_registro VARCHAR(16) PRIMARY KEY,
+    crm_medico VARCHAR(10) NOT NULL,
+    id_disponibilidade VARCHAR(16),
+    CONSTRAINT fk_registros_medico
+    FOREIGN KEY(crm_medico) REFERENCES medicos(crm),
+    CONSTRAINT fk_disponibilidade_medicos
+    FOREIGN KEY(id_disponibilidade) REFERENCES disponibilidade(id_disponibilidade)
+    
+
+
+
+
+
+)CHARSET = utf8mb4;
+
+
+CREATE TABLE consultas(
+
+	id_consulta VARCHAR(16) PRIMARY KEY,
+    crm_medico VARCHAR(10) NOT NULL,
+    cpf_paciente VARCHAR(14) NOT NULL,
+    data_consulta DATE NOT NULL,
+    hora TIME NOT NULL,
+    CONSTRAINT fk_medico_consulta
+    FOREIGN KEY(crm_medico) REFERENCES medicos(crm),
+    CONSTRAINT fk_paciente_consulta
+    FOREIGN KEY(cpf_paciente) REFERENCES pacientes(cpf)
+    
+    
+    
+    
+
+
+
+
+
+)CHARSET = utf8mb4;
+
+
---
+++ banco_de_dados/funcoes_e_triggers.sql
@@ -0,0 +1,57 @@
+
+DELIMITER $$
+
+CREATE TRIGGER exclue_disponibilidade
+BEFORE DELETE 
+ON disponibilidade
+FOR EACH ROW
+BEGIN
+
+DELETE FROM registra WHERE id_disponibilidade = OLD.id_disponibilidade;
+
+
+
+END $$
+
+
+
+DELIMITER $$
+
+
+DELIMITER $$
+
+CREATE TRIGGER reserva_disponibilidade
+AFTER INSERT 
+ON consultas
+FOR EACH ROW
+BEGIN
+
+DELETE FROM disponibilidade WHERE id_disponibilidade = NEW.id_consulta;
+
+END $$
+DELIMITER $$
+
+
+
+
+INSERT INTO pacientes (cpf, nome, sexo, senha_de_acesso, email, data_nascimento)
+VALUES (
+    '123.456.789-00',
+    'Carlos Mendes',
+    'masculino',
+    '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', -- hash SHA-256 de exemplo
+    'carlos.mendes@email.com',
+    '1990-08-15'
+);
+
+-- Inserindo uma consulta
+INSERT INTO consultas (id_consulta, crm_medico, cpf_paciente, data_consulta, hora)
+VALUES (
+    'DISP20250601A',
+    '987654RJ',
+    '123.456.789-00',
+    '2025-06-03',
+    '10:30:00'
+);
+
+
