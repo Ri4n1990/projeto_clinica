e085d5c7589be43b5c95395d2330f8f5d4c75f59
Magnus <rianmagnus5791@gmail.com>
2025-06-14 15:19:41 -0300

    adicionando validação a tela de login

--- site_clinica/estaticos/verificacao_login.js
+++ site_clinica/estaticos/verificacao_login.js
@@ -1,11 +1,17 @@
 const input_cpf = document.getElementById('cpf_paciente')
 const input_senha = document.getElementById('senha_paciente')
 const icone_visibilidade = document.getElementById('visibilidade_senha')
+const btn_login = document.getElementById('btn_login')
+const formulario = document.getElementById('formulario')
 
-input_cpf;addEventListener('input',()=>{
+input_cpf.setCustomValidity('')
+input_senha.setCustomValidity('')
+
+
+
+input_cpf.addEventListener('input',()=>{
     const regex = /\D/g
     var valor_cpf = input_cpf.value
-    
     valor_cpf = valor_cpf.replace(regex,'') 
 
     
@@ -17,17 +23,93 @@ input_cpf;addEventListener('input',()=>{
 
     input_cpf.value = valor_cpf
     
-    
 
 })
 
 
-icone_visibilidade.addEventListener('click',()=>{
+
+
+btn_login.addEventListener('click',(evt)=>{
+
+    evt.preventDefault()
     
-    icone_visibilidade.classList.toggle('senha_visivel')
-    console.log(icone_visibilidade.classList.contains('senha_visivel'))
-    input_senha.type = icone_visibilidade.classList.contains('senha_visivel') ? 'text': 'password'
+    let cpf = document.getElementById('cpf_paciente')
+    let senha = document.getElementById('senha_paciente')
+
+    let campo_cpf = document.getElementById('campo_cpf')
+    let campo_senha = document.getElementById('campo_senha')
+
+    cpf.validity.valid ? campo_cpf.classList.remove('campo_invalido') : campo_cpf.classList.add('campo_invalido')
+
+    senha.validity.valid ? campo_senha.classList.remove('campo_invalido') : campo_senha.classList.add('campo_invalido')
+
+    cpf.placeholder = campo_cpf.classList.contains('campo_invalido') ? 'Didíte um CPF válido!' : 'Insira seu CPF'
+
+    senha.placeholder = campo_senha.classList.contains('campo_invalido') ? ' Mínimo: 8 caracteres' : 'Insira sua senha'
+
+    cpf.validity.valid ? null : cpf.value = ""
+
+    senha.validity.valid ? null : senha.value = ""
+
+    cpf.validity.valid && cpf.validity.valid ? formulario.submit() : null
+
+
     
 
     
+
+
+
+
+
+
+
+
 })
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+    
+    
+
+
+    
+
+    
+
+
+
+
